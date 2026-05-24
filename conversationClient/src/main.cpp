ccdc1e7d88de20f296b436b1e375b6a80fa0034f
Ondresss <ondra.mic2@gmail.com>
2026-05-24 09:18:58 -0500

    stuff

--- conversationClient/src/main.cpp
+++ conversationClient/src/main.cpp
@@ -8,7 +8,12 @@ int main(int argc,const char** argv) {
     try
     {
         argparse::ArgumentParser program("conversationClient", "1.0");
+        program.add_description("Voice assistant client for interactive toy. Captures audio and sends it to the server.");
 
+        program.add_epilog("Examples of usage:\n"
+                           "  ./conversationClient -th 0.05 -port 9999\n"
+                           "  ./conversationClient --ip 192.168.1.50 -port 8080\n"
+                           "  ./conversationClient --ip 192.168.1.50 -port 8080 -sr 16000 -noChannels 1 -eoSentenceTh 45");
         program.add_argument("-th","--th","-noiceThreshold").help("Silence and noice threshold value").default_value(0.02).scan<'f',float>();
         program.add_argument("-ip","--ip").help("Specify IP address of the server").default_value(std::string("0.0.0.0"));
         program.add_argument("-port","--port").help("Specify port of the server").default_value(9999).scan<'i',int>();
